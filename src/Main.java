60720b5c75cddf126e11ec2407defb9ee780a25d
timothychu99 <timothychu99@gmail.com>
2023-10-15 00:05:18 -0700

    added controller

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,27 @@
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    @FXML
+    private Button updateBtn;
+
+    @FXML
+    private void updateBtnClicked() {
+        System.out.println("updateButton clicked");
+    }
+
     @Override
     public void start(Stage primaryStage) {
         try{
             Parent root = FXMLLoader.load(getClass().getResource("/ui/Contact.fxml"));
+            FXMLLoader loader = new FXMLLoader();
+            loader.setController(new Main());
             Scene scene = new Scene(root);
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -16,4 +29,5 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
+
 }
